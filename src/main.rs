3fe9a7873c8a83d625b0d256be59a87f47723512
tah-dah <markcfranz13@gmail.com>
2022-08-23 15:41:33 -0500

    fill even vec and printed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 extern crate unicode_segmentation;
 use unicode_segmentation::UnicodeSegmentation;
 fn main() {
-    // let  mut i = 1;
-    // while i < 21 {
-    //     println!("{}", i);
-    //     i = i+ 1;
-    // }
+    
     let mut i = 12;
     while i >= -14 {
         if (i % 2) == 0 {
@@ -24,7 +20,20 @@ fn main() {
         println!("{}", c);
     }
     
+    let mut even_vec:Vec<i8> = vec![];
+    //let odd_vec:Vec<i8> = vec![];
+    let full_vec:Vec<i8> = vec![2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
     
+    for e  in full_vec {
+        if e % 2 == 1 { 
+            even_vec.push(e);
+            println!("{}", e);
+    } else {
+        continue;
+    }
+
+    
+}
 
 }
 
